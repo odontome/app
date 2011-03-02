a2480cae0c8c75df3988fdd8be8ca6978ff31d19
Francisco Peña Torres <cyberf@fran-imac.local>
2011-03-02 18:15:44 +0100

    Odonto.me... ahora en español!

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,4 @@
 FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
-FastGettext.default_available_locales = ['en','es'] #all you want to allow
+FastGettext.default_available_locales = ['es'] #all you want to allow
 FastGettext.default_text_domain = 'app'
+FastGettext.locale = 'es'
