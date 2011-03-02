913ded6ae58280f3c311b3989cf61b5b4fb4e897
Raul Riera <rieraraul@gmail.com>
2011-03-02 18:41:45 +0100

    More work on the appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,10 +2,20 @@ class AppointmentsController < ApplicationController
   # filters
   before_filter :require_user
   
+  # provides
+  respond_to :html, :json
+  
+  #layout
+  layout nil
+  
   def index
      @appointments = Appointment.where("starts_at = ? AND ends_at = ? AND practice_id = ?", params[:start], params[:end], UserSession.find.user.practice_id).includes(:patients).order( "starts_at desc")
      
-     
+     respond_with(@appointments)
+  end
+  
+  def new
+    
   end
 
 end
