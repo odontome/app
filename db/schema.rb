72ea28dba756039b134b996cc07fb9db1b74b234
Raul Riera <rieraraul@gmail.com>
2011-03-02 01:07:37 +0100

    More changes... tired of this.. :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110301123149) do
+ActiveRecord::Schema.define(:version => 20110301165436) do
 
   create_table "appointments", :force => true do |t|
     t.integer  "practice_id",               :null => false
