ee84a9fd5f29d964eab5d544efe7313548bbd0e6
Raul Riera <rieraraul@gmail.com>
2011-03-04 01:05:20 +0100

    Moved down the patient search so it can be localized

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,10 +3,7 @@ $(function(){
 	$('input').tipTip();	
 	$('textarea').tipTip();
 	$('select').tipTip();
-		
-	// Patient search box
-	$("#patient-search").autoSuggest("/patients/search", {startText: "Enter the patient name or UID", minChars: 2, selectionLimit: 1, selectedValuesProp: "data", selectedItemProp: "name", searchObjProps: "value,name", resultClick: function(data){ window.location.href = "/patients/show/" + data.attributes.data; }});
-	
+			
 	// This function prevents the session from ending
 	window.iCallServerId = setInterval(function (){ var remoteURL = '/'; $.get(remoteURL); }, 900000);
 });	
