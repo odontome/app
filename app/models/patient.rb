4aed24f411efb95666e8534e32c007efa2cb7399
Raul Riera <rieraraul@gmail.com>
2011-03-10 02:09:03 +0100

    UI changes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -8,8 +8,8 @@ class Patient < ActiveRecord::Base
     where("patients.practice_id = ? ", UserSession.find.user.practice_id)
   }  
 
-  validates_uniqueness_of :uid, :scope => :practice_id, :allow_nil => true
-  validates_presence_of :practice_id, :firstname, :lastname, :date_of_birth
+  validates_uniqueness_of :uid, :scope => :practice_id, :allow_nil => true, :allow_blank => true
+  validates_presence_of :practice_id, :firstname, :lastname
   validates_length_of :uid, :within => 0..25, :allow_blank => true
   validates_length_of :firstname, :within => 1..25
   validates_length_of :lastname, :within => 1..25
@@ -17,7 +17,7 @@ class Patient < ActiveRecord::Base
   validates_length_of :telephone, :within => 0..20, :allow_blank => true
   validates_length_of :mobile, :within => 0..20, :allow_blank => true
   validates_length_of :emergency_telephone, :within => 0..20, :allow_blank => true
-  validates_format_of :email, :with => Authlogic::Regex.email
+  validates_format_of :email, :with => Authlogic::Regex.email, :allow_blank => true
 
   before_validation(:on => :create) do
     set_practice_id
@@ -25,6 +25,12 @@ class Patient < ActiveRecord::Base
 
   def fullname
     [firstname, lastname].join(' ')
+  end
+  
+  def fullname=(name)
+    split = name.split(' ', 2)
+    self.firstname = split.first
+    self.lastname = split.last
   end
 
   def age
