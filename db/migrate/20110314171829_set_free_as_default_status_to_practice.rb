b82999bc06c6b2a27afa8412211bdb87343f618f
Francisco Peña Torres <cyberf@fran-imac.local>
2011-03-15 16:56:21 +0100

    Error en Posgrel con migrate

--- db/migrate/20110314171829_set_free_as_default_status_to_practice.rb
+++ db/migrate/20110314171829_set_free_as_default_status_to_practice.rb
@@ -2,14 +2,14 @@ class SetFreeAsDefaultStatusToPractice < ActiveRecord::Migration
   def self.up
     change_table :practices do |t|
       t.change :status, :string, :limit => 50, :default => "free"
-      Practice.connection.execute('update practices set status="free" where status="unconfirmed"')
+      Practice.connection.execute("update practices set status='free' where status='unconfirmed'")
     end
   end
 
   def self.down
     change_table :practices do |t|
       t.change :status, :string, :limit => 50, :default => "unconfirmed"
-      Practice.connection.execute('update practices set status="unconfirmed" where status="free"')
+      Practice.connection.execute("update practices set status='unconfirmed' where status='free'")
     end
   end
 end
