d7f302af0fc0b7925574a435050838a18a68a783
Fran <cyberf@cyberf.org>
2011-04-01 18:03:10 +0200

    Pruebas de mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Odontome::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Email configuration
+  config.action_mailer.delivery_method = :smtp
+  
 end
