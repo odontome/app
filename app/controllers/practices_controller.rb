5d0f55bfb7036c2b553a6c162da4f2380a6b526e
Fran <cyberf@cyberf.org>
2011-04-01 18:27:41 +0200

    Más para pruebas de mailing

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -28,6 +28,7 @@ class PracticesController < ApplicationController
     
     respond_to do |format|
       if @practice.save
+        PracticeMailer.welcome_email(@practice).deliver
         format.html { redirect_to(practice_path, :notice => _('Your practice is now active! You can start to set everything up in here.')) }
       else
         format.html { render :action => "new", :as => :signup }
