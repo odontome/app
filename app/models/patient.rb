d9f397fbf54f91853270d9fe3bc7f162cd46622d
Raul Riera <rieraraul@gmail.com>
2011-04-01 21:22:32 +0200

    Duplicated validation

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -21,7 +21,7 @@ class Patient < ActiveRecord::Base
   
   # validations
   validates_uniqueness_of :uid, :scope => :practice_id, :allow_nil => true, :allow_blank => true
-  validates_presence_of :practice_id, :firstname, :lastname, :date_of_birth, :past_illnesses, :past_illnesses, :surgeries, :medications, :drugs_use, :family_diseases
+  validates_presence_of :practice_id, :firstname, :lastname, :date_of_birth, :past_illnesses, :surgeries, :medications, :drugs_use, :family_diseases
   validates_numericality_of :cigarettes_per_day, :drinks_per_day, :only_integer => true, :greater_than_or_equal_to => 0
   validates_length_of :uid, :within => 0..25, :allow_blank => true
   validates_length_of :firstname, :within => 1..25
