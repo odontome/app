b7f14ef6256be321ab4a3b1175f47a6deb2eb08e
Raul Riera <rieraraul@gmail.com>
2011-04-14 16:16:36 +0200

    Now the balance uses the new hacked currency function, plus bug fix

--- config/initializers/hacked_helpers.rb
+++ config/initializers/hacked_helpers.rb
@@ -3,8 +3,8 @@ module ActionView
     module NumberHelper
       def number_to_currency_with_currency(number, options = {})
         defaults = {:unit => ''}
-        s = number_to_currency_without_currency(number, defaults.merge(options))
-        s << @current_user.practice.currency_unit unless options[:unit]
+        s = @current_user.practice.currency_unit unless options[:unit]
+        s << number_to_currency_without_currency(number, defaults.merge(options))
       end
       alias_method_chain :number_to_currency, :currency
     end
