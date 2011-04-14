47788f45016599b4c549b1602e5b3f8c6091ead9
Fran <cyberf@cyberf.org>
2011-04-14 15:30:22 +0200

    UserVoice installed.

---
+++ public/javascripts/uservoice.js
@@ -0,0 +1,5 @@
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/4baBQ9pKproCl9DkiWwgg.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
