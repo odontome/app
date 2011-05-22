c8d628bb0bb61c52ddbfe0e1d8fe82b938aceadb
Fran <cyberf@cyberf.org>
2011-05-22 23:34:57 +0200

    Prueba de correción de problema de que no muestra el flash incial al crear la práctica

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -33,7 +33,8 @@ class PracticesController < ApplicationController
     respond_to do |format|
       if @practice.save
         PracticeMailer.welcome_email(@practice).deliver
-        format.html { redirect_to(practice_path, :notice => _('Your practice is now active! You can start to set everything up in here.')) }
+        flash[:notice] = _('Your practice is now active! You can start to set everything up in here.')
+        format.html { redirect_to practice_path }
       else
         format.html { render :action => "new", :as => :signup, :layout => "user_sessions" }
       end
