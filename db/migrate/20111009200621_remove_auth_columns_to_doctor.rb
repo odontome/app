2d18f5e43dc22fc78d101840dc53325c706c7301
Raul Riera <rieraraul@gmail.com>
2011-10-09 22:10:23 +0200

    We won't be needing "logins" for doctors

---
+++ db/migrate/20111009200621_remove_auth_columns_to_doctor.rb
@@ -0,0 +1,23 @@
+class RemoveAuthColumnsToDoctor < ActiveRecord::Migration
+  def self.up
+  	remove_column :doctors, :persistence_token
+    remove_column :doctors, :crypted_password
+  	remove_column :doctors, :password_salt
+  	remove_column :doctors, :single_access_token
+  	remove_column :doctors, :current_login_at
+  	remove_column :doctors, :last_login_at
+  	remove_column :doctors, :login_count
+   end
+
+  def self.down
+		change_table :doctors do |t|
+		  t.string    :crypted_password
+		  t.string    :password_salt
+		  t.string    :persistence_token
+		  t.string    :single_access_token
+		  t.datetime  :current_login_at
+		  t.datetime  :last_login_at
+		  t.integer   :login_count, :null => false, :default => 0
+		end
+  end
+end
