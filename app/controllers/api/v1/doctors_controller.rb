b5526a9963eded46c31bfbce1625b85c2177479e
Raul Riera <rieraraul@gmail.com>
2011-10-10 21:44:15 +0200

    Now the API just returns valid doctors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DoctorsController < Api::V1::BaseController
   before_filter :find_doctor, :only => [:show]
   
   def index
-    respond_with(Doctor.mine, :methods => "fullname")
+    respond_with(Doctor.mine.valid, :methods => "fullname")
   end
   
   def show
@@ -13,7 +13,7 @@ class Api::V1::DoctorsController < Api::V1::BaseController
   private
   
   def find_doctor
-  	@doctor = Doctor.mine.find(params[:id])
+  	@doctor = Doctor.mine.valid.find(params[:id])
   	rescue ActiveRecord::RecordNotFound
   		error = { :error => "The doctor you were looking for could not be found."}
   		respond_with(error, :status => 404)
