2a78ad70dbf5b12580e3463104e1df48b8ff372f
Raul Riera <rieraraul@gmail.com>
2012-02-12 06:04:09 +0100

    Another attempt at the reminder bug

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -4,7 +4,6 @@ class PatientMailer < ActionMailer::Base
   default :from => "hello@odonto.me"
   
   def appointment_soon_email(patient_email, patient_firstname, patient_lastname, start_time, end_time, practice_name, practice_locale, doctor_firstname, doctor_lastname)
-    set_locale(practice_locale)
     @patient_firstname = patient_firstname
     @patient_lastname = patient_lastname
     @start_time = start_time
@@ -13,10 +12,6 @@ class PatientMailer < ActionMailer::Base
     @doctor_firstname = doctor_firstname
     @doctor_lastname = doctor_lastname
     mail(:to => patient_email, :subject => _("You have an appointment soon in") + " " + practice_name)
-  end
-  
-  def set_locale(practice_locale)
-    I18n.locale = FastGettext.set_locale(practice_locale)
   end
 
 end
