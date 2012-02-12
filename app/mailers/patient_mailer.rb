924161b6304dfcb1cde0b4322f5346fbb5de2fb3
Raul Riera <rieraraul@gmail.com>
2012-02-12 06:28:56 +0100

    Improved reminder message

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -3,15 +3,13 @@ class PatientMailer < ActionMailer::Base
   layout 'email'
   default :from => "hello@odonto.me"
   
-  def appointment_soon_email(patient_email, patient_firstname, patient_lastname, start_time, end_time, practice_name, practice_locale, doctor_firstname, doctor_lastname)
+  def appointment_soon_email(patient_email, patient_name, start_time, end_time, practice_name, practice_locale, doctor_name)
     set_locale(practice_locale)
-    @patient_firstname = patient_firstname
-    @patient_lastname = patient_lastname
+    @patient_name = patient_name
     @start_time = start_time
     @end_time = end_time
     @practice_name = practice_name
-    @doctor_firstname = doctor_firstname
-    @doctor_lastname = doctor_lastname
+    @doctor_name = doctor_name
     mail(:to => patient_email, :subject => _("You have an appointment soon in") + " " + practice_name)
   end
   
