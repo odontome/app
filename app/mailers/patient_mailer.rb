0837b7c9fee3b583fb1255598653213e6295ee3e
Raul Riera <rieraraul@gmail.com>
2012-02-12 06:10:07 +0100

    That didn't do anything

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -4,6 +4,7 @@ class PatientMailer < ActionMailer::Base
   default :from => "hello@odonto.me"
   
   def appointment_soon_email(patient_email, patient_firstname, patient_lastname, start_time, end_time, practice_name, practice_locale, doctor_firstname, doctor_lastname)
+    set_locale(practice_locale)
     @patient_firstname = patient_firstname
     @patient_lastname = patient_lastname
     @start_time = start_time
@@ -12,6 +13,10 @@ class PatientMailer < ActionMailer::Base
     @doctor_firstname = doctor_firstname
     @doctor_lastname = doctor_lastname
     mail(:to => patient_email, :subject => _("You have an appointment soon in") + " " + practice_name)
+  end
+  
+  def set_locale(practice_locale)
+    I18n.locale = FastGettext.set_locale(practice_locale)
   end
 
 end
