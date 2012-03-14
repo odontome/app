f56e91a80d364ef705c789b2a4bfa821fbbdd6ea
Raul Riera <rieraraul@gmail.com>
2012-03-14 21:44:42 +0100

    Removed everything related to "Paypal"

--- config/routes.rb
+++ config/routes.rb
@@ -40,9 +40,6 @@ Odontome::Application.routes.draw do
   match "/practice/cancel" => "practices#cancel", :as => :practice_cancel
   match "/practice/change_to_free_plan" => "practices#change_to_free_plan", :as => :change_to_free_plan
   match "/practice/close" => "practices#close", :as => :practice_close, :via => :post
-  match "/paypal_ipn" => "paypal#paypal_ipn", :as => :paypal_ipn, :via => :post
-  match "/paypal/cancel" => "paypal#cancel"
-  match "/paypal/success" => "paypal#success"
   match "/set_session_time_zone"  => "welcome#set_session_time_zone"
   match "/privacy"  => "welcome#privacy"
   match "/terms"  => "welcome#terms"
