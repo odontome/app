9166d282a6384259386a85faf8f904407fcd923c
Raúl Riera <rieraraul@gmail.com>
2012-06-02 20:40:32 +0200

    Replaced the jqgrid thingy for something simpler

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,26 +1,11 @@
 class AdminController < ApplicationController
 
   before_filter :require_superadmin
-  respond_to :html,:json
+  respond_to :html
 
   def practices
-    index_columns ||= [:id,:name,:locale,:timezone, :patients_count, :appointments_count, :doctors_count, :users_count]
-    current_page = params[:page] ? params[:page].to_i : 1
-    rows_per_page = params[:rows] ? params[:rows].to_i : 10
+    @practices = Practice.select("id, name, created_at, patients_count, appointments_count, doctors_count, users_count")
 
-    conditions={:page => current_page, :per_page => rows_per_page}
-    conditions[:order] = params["sidx"] + " " + params["sord"] unless (params[:sidx].blank? || params[:sord].blank?)
-  
-    if params[:_search] == "true"
-      conditions[:conditions]=filter_by_conditions(index_columns)
-    end
-  
-    @practices=Practice.paginate(conditions)
-    total_entries=@practices.total_entries
-  
-    respond_with(@practices) do |format|
-      format.json { render :json => @practices.to_jqgrid_json(index_columns, current_page, rows_per_page, total_entries)}  
-    end
   end
   
 end
--- vendor/plugins/jqgrid-rails3/init.rb
+++
@@ -1,5 +0,0 @@
-require '2dc_jqgrid'
-
-Array.send :include, JqgridJson
-ActionView::Base.send :include, Jqgrid
-ActionController::Base.send :include,JqgridFilter
