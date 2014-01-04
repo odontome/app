d90a43c5e25c615b96657225e2fa4dca002476d1
Raúl Riera <rieraraul@gmail.com>
2014-01-04 00:43:55 +0000

    Adding defaults timezone and locale, should be detected from the user (like the old days)

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -13,9 +13,10 @@ class Practice < ActiveRecord::Base
   accepts_nested_attributes_for :users, :limit => 1
   
   # validations
-  validates_presence_of :name
+  validates_presence_of :name, :timezone, :locale
 
   # callbacks
+  before_validation :set_timezone_and_locale
   before_validation :set_first_user_data, :on => :create
 
   def set_as_cancelled
@@ -31,6 +32,11 @@ class Practice < ActiveRecord::Base
 
   private
   
+  def set_timezone_and_locale
+    self.timezone = Time.zone.name
+    self.locale = "en"
+  end
+
   def set_first_user_data
     self.users.first.firstname = I18n.t :administrator
     self.users.first.lastname = (I18n.t :user).downcase
