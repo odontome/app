ddde4ae99982ca73408e1974ab7c1c6274aaa291
Raúl Riera <rieraraul@gmail.com>
2014-01-18 17:40:08 +0000

    Trying out the new design (not fully implemented.. but quite there)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require_self
-//= require_tree .
+//= require bootstrap
+//= require fullcalendar.min
+//= require jquery-ui-1.10.2.custom.min
+//= require jquery.autoSuggest
+//= require theme
 
+// This function prevents the session from ending
+window.iCallServerId = setInterval(function (){ var remoteURL = '/'; $.get(remoteURL); }, 900000);
