a7c99c0de53af2181bf56546cb5217caf60cca83
Raúl Riera <rieraraul@gmail.com>
2014-01-24 13:09:37 +0100

    Added a migration to update the content of the "locale"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140118170341) do
+ActiveRecord::Schema.define(:version => 20140124120337) do
 
   create_table "appointments", :force => true do |t|
     t.integer  "doctor_id"
