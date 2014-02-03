97799dbc139832a4880d5891eace56924d4abec4
Raúl Riera <rieraraul@gmail.com>
2014-02-03 21:58:58 +0000

    Fixed typos

--- db/migrate/20140118170341_update_counters_in_practices.rb
+++ db/migrate/20140118170341_update_counters_in_practices.rb
@@ -1,11 +1,11 @@
 class UpdateCountersInPractices < ActiveRecord::Migration
   def up
   	remove_column :practices, :appointments_count
-  	add_column :practices, :datebooks_count, :interger
+  	add_column :practices, :datebooks_count, :integer
   end
 
   def down
   	remove_column :practices, :datebooks_count
-  	add_column :practices, :appointments_count, :interger
+  	add_column :practices, :appointments_count, :integer
   end
 end
