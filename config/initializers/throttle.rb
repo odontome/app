4559659095331f27a5b9d54b0c023955debefc30
Raúl Riera <rieraraul@gmail.com>
2014-02-03 22:10:35 +0000

    Removing the asset pipeline from the ignore. Also fixes a bug with Redis

--- config/initializers/throttle.rb
+++ config/initializers/throttle.rb
@@ -1,7 +1,9 @@
 require "redis"
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV.include?('REDISTOGO_URL')
+	uri = URI.parse(ENV["REDISTOGO_URL"])
+	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 THROTTLE_TIME_WINDOW = 60 * 60
 THROTTLE_MAX_REQUESTS = 50
