a2e6a92234fb3e7c9c192c3040147f7c5decaa6c
Raúl Riera <rieraraul@gmail.com>
2014-02-03 22:00:27 +0000

    More removal of ==

--- db/migrate/20140124120337_update_practices_locale.rb
+++ db/migrate/20140124120337_update_practices_locale.rb
@@ -1,11 +1,11 @@
 class UpdatePracticesLocale < ActiveRecord::Migration
   def up
-  	Practice.update_all "locale = 'es'", "locale == 'es_ES'"
-  	Practice.update_all "locale = 'en'", "locale == 'en_US'"
+  	Practice.update_all "locale = 'es'", "locale = 'es_ES'"
+  	Practice.update_all "locale = 'en'", "locale = 'en_US'"
   end
 
   def down
-  	Practice.update_all "locale = 'es_ES'", "locale == 'es'"
-  	Practice.update_all "locale = 'en_US'", "locale == 'en'"
+  	Practice.update_all "locale = 'es_ES'", "locale = 'es'"
+  	Practice.update_all "locale = 'en_US'", "locale = 'en'"
   end
 end
