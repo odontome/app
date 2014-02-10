db35cea1d81d6145ccb2ee5212a73ddbba092122
Raúl Riera <rieraraul@gmail.com>
2014-02-10 20:53:33 +0000

    Fixed a bug in the jquery.ui.touch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 //= require bootstrap
 //= require fullcalendar.min
 //= require jquery-ui-1.10.2.custom.min
-//= require jquery.ui.touch-punch
+//= require jquery.ui.touch
 //= require jquery.autoSuggest
 //= require jquery.minicolors
 //= require retina-1.1.0
