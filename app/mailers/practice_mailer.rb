0de29a65df2f928ce2b38b8849044ce345783691
Raúl Riera <rieraraul@gmail.com>
2014-03-16 17:31:01 +0000

    Improvement in the branding of our emails (the ones not seen by patients)

--- app/mailers/practice_mailer.rb
+++ app/mailers/practice_mailer.rb
@@ -4,6 +4,8 @@ class PracticeMailer < ActionMailer::Base
   default :from => "Odonto.me <hello@odonto.me>"
   
   def welcome_email(practice)
+  	@show_logo_in_header = true
+
   	mail(:to => practice.users.first.email, :subject => I18n.t("mailers.practice.welcome.subject"))
   end
 
