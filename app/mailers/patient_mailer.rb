0b32dbbf0766e4c8230ca7c3741fe319f76b8b4e
Raúl Riera <rieraraul@gmail.com>
2014-04-02 22:20:13 +0100

    Fixed the dates of the emails, also refactored the code to make it easier to edit

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -3,17 +3,18 @@ class PatientMailer < ActionMailer::Base
   layout 'email'
 
   def appointment_soon_email(patient_email, patient_name, start_time, end_time, practice_name, practice_locale, practice_timezone, doctor_name, practice_email)
-    
-    @patient_name = patient_name
-    @start_time = start_time
-    @end_time = end_time
-    @practice_name = practice_name
-    @practice_timezone = practice_timezone
-    @doctor_name = doctor_name
-    
+        
     # temporarely set the locale and then change it back
     # when the block finishes
     I18n.with_locale(practice_locale) do
+      @patient_name = patient_name      
+      @practice_name = practice_name
+      @practice_timezone = practice_timezone
+      @doctor_name = doctor_name
+      @appointment_date = I18n.l start_time.in_time_zone(@practice_timezone).to_date, :format => :day_and_date
+      @appointment_start_time = I18n.l start_time.to_time.in_time_zone(@practice_timezone), :format => :just_the_time
+      @appointment_end_time = I18n.l end_time.to_time.in_time_zone(@practice_timezone), :format => :just_the_time
+
       mail(:from => "#{practice_name} <hello@odonto.me>", 
            :to => patient_email, 
            :subject => I18n.t("mailers.patient.appointment_soon_email.subject", practice_name: practice_name), 
@@ -23,17 +24,18 @@ class PatientMailer < ActionMailer::Base
 
   def appointment_scheduled_email(patient_email, patient_name, start_time, end_time, practice_name, practice_locale, practice_timezone, doctor_name, practice_email, passbook_url)
         
-    @patient_name = patient_name
-    @start_time = start_time
-    @end_time = end_time
-    @practice_name = practice_name
-    @practice_timezone = practice_timezone
-    @doctor_name = doctor_name
-    @passbook_url = passbook_url
-    
     # temporarely set the locale and then change it back
     # when the block finishes
     I18n.with_locale(practice_locale) do
+      @patient_name = patient_name      
+      @practice_name = practice_name
+      @practice_timezone = practice_timezone
+      @doctor_name = doctor_name
+      @passbook_url = passbook_url
+      @appointment_date = I18n.l start_time.in_time_zone(@practice_timezone).to_date, :format => :day_and_date
+      @appointment_start_time = I18n.l start_time.to_time.in_time_zone(@practice_timezone), :format => :just_the_time
+      @appointment_end_time = I18n.l end_time.to_time.in_time_zone(@practice_timezone), :format => :just_the_time
+
       mail(:from => "#{practice_name} <hello@odonto.me>", 
            :to => patient_email, 
            :subject => I18n.t("mailers.patient.appointment_scheduled_email.subject", practice_name: practice_name), 
