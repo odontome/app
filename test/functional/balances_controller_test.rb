36af864711b59e2c457080894837b34577da009c
Raúl Riera <rieraraul@gmail.com>
2014-05-12 22:26:09 +0100

    Basic test case

--- test/functional/balances_controller_test.rb
+++ test/functional/balances_controller_test.rb
@@ -1,11 +1,37 @@
 require 'test_helper'
 
 class BalancesControllerTest < ActionController::TestCase
-  
+
   setup do
   	UserSession.create users(:founder)
-  	
-  	@balance = { :amount => 9.99, :patient_id => 1 }
   end
-  
+
+  test "should get index" do
+    get :index, patient_id: 1
+    assert_response :success
+    assert_not_nil assigns(:treatments)
+  end
+
+  test "should create an income entry" do
+    entry = {
+      :amount => 9.99,
+      :notes => 'Can of soda'
+    }
+
+    assert_difference 'Balance.count' do
+      post :create, { balance: entry, patient_id: 1 }
+    end
+  end
+
+  test "should create an expense entry" do
+    entry = {
+      :amount => -9.99,
+      :notes => 'Returned the can of soda'
+    }
+
+    assert_difference 'Balance.count' do
+      post :create, { balance: entry, patient_id: 1 }
+    end
+  end
+
 end
