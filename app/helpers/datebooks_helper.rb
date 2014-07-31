b17329d2e49f1a6d2307307e624968f888afb1f9
Raul Riera <rieraraul@gmail.com>
2014-07-31 21:11:48 +0100

    Casting the number to ints before trying to use the "hours" method

--- app/helpers/datebooks_helper.rb
+++ app/helpers/datebooks_helper.rb
@@ -1,8 +1,8 @@
 module DatebooksHelper
   def hours_range(starts_at, ends_at)
     # turn the integers into a date
-    starts_at = DateTime.now.utc.beginning_of_day + starts_at.hours
-    ends_at = DateTime.now.utc.beginning_of_day + ends_at.hours
+    starts_at = DateTime.now.utc.beginning_of_day + starts_at.to_i.hours
+    ends_at = DateTime.now.utc.beginning_of_day + ends_at.to_i.hours
 
     # format the date to show only the hours
     starts_at = starts_at.strftime("%H:%M")
