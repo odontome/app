a82adf5ed039122c10a3d21ca659e5d42b20e173
Raul Riera <rieraraul@gmail.com>
2014-08-21 21:09:28 +0100

    Fixed a bug in the tests

--- test/functional/datebooks_controller_test.rb
+++ test/functional/datebooks_controller_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class DatebooksControllerTest < ActionController::TestCase
-  
+
   setup do
   	UserSession.create users(:founder)
-  	
+
   	@datebook = { :name => "Bokanova Dental" }
   end
-  
+
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:datebooks)
   end
-  
+
 	test "should get new" do
 	  get :new
 	  assert_response :success
@@ -25,19 +25,19 @@ class DatebooksControllerTest < ActionController::TestCase
     get :new
     assert_response :redirect
   end
-  
+
   test "should create datebook" do
     assert_difference('Datebook.count') do
       post :create, datebook: @datebook
     end
     assert_redirected_to datebooks_url
   end
-  
+
   test "should show datebook" do
     get :show, id: datebooks(:playa_del_carmen).to_param
     assert_response :success
   end
-  
+
   test "should get edit" do
     get :edit, id: datebooks(:playa_del_carmen).to_param
     assert_response :success
@@ -46,10 +46,10 @@ class DatebooksControllerTest < ActionController::TestCase
   test "should not get edit if not admin" do
     UserSession.create users(:perishable)
 
-    get :edit
+    get :edit, id: datebooks(:playa_del_carmen).to_param
     assert_response :redirect
   end
-  
+
   test "should update datebook" do
     put :update, id: datebooks(:playa_del_carmen).to_param, datebook: @datebook
     assert_redirected_to datebooks_url
@@ -59,7 +59,7 @@ class DatebooksControllerTest < ActionController::TestCase
     assert_difference('Datebook.count', -1) do
       delete :destroy, id: datebooks(:without_appointments).to_param
     end
-				
+
     assert_redirected_to datebooks_url
   end
 
@@ -67,7 +67,7 @@ class DatebooksControllerTest < ActionController::TestCase
     assert_no_difference('Datebook.count') do
       delete :destroy, id: datebooks(:playa_del_carmen).to_param
     end
-        
+
     assert_redirected_to datebooks_url
   end
 
