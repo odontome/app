e380fd075e017ba0f7f2de51d619bb16b7c782af
Raul Riera <rieraraul@gmail.com>
2014-10-11 18:10:24 +0100

    Removed "link tracking" from the reset password email. Added a test

---
+++ test/unit/user_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+
+  test "reset password instructions mail" do
+    user = users(:founder)
+
+    # Send the email, then test that it got queued
+    email = user.deliver_password_reset_instructions!
+
+    assert !ActionMailer::Base.deliveries.empty?
+
+    # Test the body of the sent email contains what we expect it to
+    assert_equal ['hello@odonto.me'], email.from
+    assert_equal ['raulriera@hotmail.com'], email.to
+    assert_equal I18n.t("mailers.notifier.password_reset.subject"), email.subject
+  end
+
+end
