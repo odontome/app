a9b328ae86c872c115f74d5173d1d480a0bcb9d7
Raul Riera <rieraraul@gmail.com>
2015-02-26 23:58:50 +0000

    Made it easier to select that long URL

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 
 // This function prevents the session from ending
 window.iCallServerId = setInterval(function (){ var remoteURL = '/'; $.get(remoteURL); }, 900000);
+
+function selectText(element) {
+  var doc = document
+      , text = element
+      , range, selection
+  ;
+  if (doc.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(text);
+      range.select();
+  } else if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+  }
+}
