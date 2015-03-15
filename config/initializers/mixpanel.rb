176d21a43013060018adce240850e98d972c6944
Raul Riera <rieraraul@gmail.com>
2015-03-15 02:04:57 +0000

    Improving mix panel data by using Javascript as well

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,7 +1,9 @@
 require 'mixpanel-ruby'
 
 if Rails.env.development? || Rails.env.test?
-  MIXPANEL_CLIENT = Mixpanel::Tracker.new("ea0b61ee9bb501bf0ff98740f0a4b0e2")
+  MIXPANEL_KEY = "ea0b61ee9bb501bf0ff98740f0a4b0e2"
 else
-  MIXPANEL_CLIENT = Mixpanel::Tracker.new("a11e3dace9deabb780160e4acde44863")
+  MIXPANEL_KEY = "a11e3dace9deabb780160e4acde44863"
 end
+
+MIXPANEL_CLIENT = Mixpanel::Tracker.new(MIXPANEL_KEY)
