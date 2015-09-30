15d8f8a836b6d4318c45a6e629b04f11b57995c7
Raul Riera <rieraraul@gmail.com>
2015-09-30 19:30:02 +0100

    Adding tracking to datebooks

--- app/controllers/datebooks_controller.rb
+++ app/controllers/datebooks_controller.rb
@@ -11,6 +11,9 @@ class DatebooksController < ApplicationController
     .joins('left outer join appointments on appointments.datebook_id = datebooks.id')
     .group('datebooks.id')
     .order('datebooks.created_at')
+
+    # track this event
+    MIXPANEL_CLIENT.track(@current_user.email, 'Viewing all datebooks')
   end
 
   def show
@@ -25,10 +28,18 @@ class DatebooksController < ApplicationController
 
     # Detect if this is coming from a mobile device
     @is_mobile = request.user_agent =~ /iPhone|webOS|Android/
+
+    # track this event
+    MIXPANEL_CLIENT.track(@current_user.email, 'Viewing a datebook', {
+        'Name' => @datebook.name
+    })
   end
 
   def new
     @datebook = Datebook.new
+
+    # track this event
+    MIXPANEL_CLIENT.track(@current_user.email, 'Creating a new datebook')
   end
 
   def create
@@ -36,6 +47,9 @@ class DatebooksController < ApplicationController
 
     respond_to do |format|
       if @datebook.save
+        # track this event
+        MIXPANEL_CLIENT.track(@current_user.email, 'Completed a datebook')
+
         format.html { redirect_to(datebooks_url, :notice => t(:datebook_created_success_message))}
       else
         format.html { render :action => "new" }
@@ -45,6 +59,9 @@ class DatebooksController < ApplicationController
 
   def edit
     @datebook = Datebook.mine.find params[:id]
+
+    # track this event
+    MIXPANEL_CLIENT.track(@current_user.email, 'Modifying a datebook')
   end
 
   def update
@@ -52,6 +69,9 @@ class DatebooksController < ApplicationController
 
     respond_to do |format|
       if @datebook.update_attributes(params[:datebook])
+        # track this event
+        MIXPANEL_CLIENT.track(@current_user.email, 'Modified a datebook')
+
         format.html { redirect_to(datebooks_url, :notice => t(:datebook_updated_success_message)) }
       else
         format.html { render :action => "edit" }
@@ -64,6 +84,11 @@ class DatebooksController < ApplicationController
 
     respond_to do |format|
       if (@datebook.destroy)
+        # track this event
+        MIXPANEL_CLIENT.track(@current_user.email, 'Deleted a datebook', {
+          'Name' => @datebook.name
+        })
+
         format.html { redirect_to(datebooks_url) }
       else
         format.html { redirect_to(datebooks_url, :error => I18n.t("errors.messages.has_appointments"))}
