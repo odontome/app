8fcde66d50e2f0491526802bfd0f029963ee3af2
Raul Riera <rieraraul@gmail.com>
2016-02-28 19:05:57 +0000

    Swapped Mandrill for Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,15 @@ Rails.application.configure do
 
   # Configure the host for emails
   ActionMailer::Base.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      ENV['MANDRILL_USERNAME'],
-      :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'odonto.me',
-      :authentication => :plain
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
+
   ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.asset_host = "http://my.odonto.me"
   config.action_mailer.default_url_options = { :host => 'my.odonto.me' }
