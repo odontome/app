0036dca5faaa217399a78d3620f1817c6378b706
Raul Riera <rieraraul@gmail.com>
2020-08-15 14:26:11 -0400

    Removed all the code for passbooks

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -20,7 +20,7 @@ class PatientMailer < ApplicationMailer
     end
   end
 
-  def appointment_scheduled_email(patient_email, patient_name, start_time, end_time, practice_name, practice_locale, practice_timezone, doctor, practice_email, passbook_url)
+  def appointment_scheduled_email(patient_email, patient_name, start_time, end_time, practice_name, practice_locale, practice_timezone, doctor, practice_email)
 
     # temporarely set the locale and then change it back
     # when the block finishes
@@ -29,7 +29,6 @@ class PatientMailer < ApplicationMailer
       @practice_name = practice_name
       @practice_timezone = practice_timezone
       @doctor_name = doctor.fullname
-      @passbook_url = passbook_url
       @appointment_date = I18n.l start_time.in_time_zone(@practice_timezone).to_date, :format => :day_and_date
       @appointment_start_time = I18n.l start_time.to_time.in_time_zone(@practice_timezone), :format => :just_the_time
       @appointment_end_time = I18n.l end_time.to_time.in_time_zone(@practice_timezone), :format => :just_the_time
--- config/initializers/passbook.rb
+++
@@ -1,8 +0,0 @@
-require 'passbook'
-
-Passbook.configure do |passbook|
-  passbook.wwdc_cert = File.join(Rails.root, "passbook", "WWDR.pem")
-  passbook.p12_key = File.join(Rails.root, "passbook", "passkey.pem")
-  passbook.p12_certificate = File.join(Rails.root, "passbook", "passcertificate.pem")
-  passbook.p12_password = 'dunces216!zapped'
-end
