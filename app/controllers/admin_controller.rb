2b629d697881b34bfa96192d8cf3cb04ec083805
Raul Riera <rieraraul@gmail.com>
2021-02-14 17:56:33 -0500

    Oops, missed one

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,5 @@
 class AdminController < ApplicationController
   before_action :require_superadmin
-  respond_to :html
 
   def practices
     @practices = Practice.select('id, name, created_at, cancelled_at, datebooks_count, patients_count, doctors_count, users_count').order('created_at desc')
