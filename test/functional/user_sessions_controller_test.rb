53cf6d357c63f91dcad6df0ecf6252eef5f16f36
Raul Riera <rieraraul@gmail.com>
2021-02-15 14:22:01 -0500

    Added a test case for the new password reset enforcement

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -9,10 +9,18 @@ class UserSessionsControllerTest < ActionController::TestCase
   end
 
   test 'should create user session if valid params are given' do
-    post :create, params: { signin: { email: 'raulriera@hotmail.com', password: '1234567890' } }
+    post :create, params: { signin: { email: users(:founder).email, password: '1234567890' } }
 
     assert_equal @controller.session['user'], users(:founder)
     assert_redirected_to root_url
+  end
+
+  test 'should request user to reset password if not migrated' do
+    non_migrated_user = users(:non_migrated_user)
+    post :create, params: { signin: { email: non_migrated_user.email, password: '1234567890' } }
+
+    assert_nil @controller.session['user']
+    assert_redirected_to new_password_reset_url
   end
 
   # FIXME: add support for this
