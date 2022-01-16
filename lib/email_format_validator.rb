1b2eae83e862bac2ef561b4ce4a64ab6ddbd2c91
Raul Riera <rieraraul@gmail.com>
2022-01-16 17:47:03 -0500

    chore: remove dead code

--- lib/email_format_validator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class EmailFormatValidator < ActiveModel::EachValidator
-  def validate_each(object, attribute, value)
-    unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-      object.errors[attribute] << (options[:message] || I18n.t('errors.messages.invalid_email_format'))
-    end
-  end
-end
