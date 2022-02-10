8381547a7b744e5bb3645eab137c438c0680cf2e
Raul Riera <rieraraul@gmail.com>
2022-02-09 19:26:10 -0500

    fix: attempt to fix the smtp issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -94,18 +94,9 @@ Rails.application.configure do
   # To configure the SSL Enforcer gem
   # config.middleware.use Rack::SslEnforcer, :only => ["/signup", "/signin", "/users/new", /^\/users\/(.+)\/edit/, "/set_session_time_zone", /^\/api\//], :mixed => true
 
-  # Configure the host for emails
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.asset_host = 'https://my.odonto.me'
   config.action_mailer.default_url_options = { host: 'my.odonto.me' }
 end
