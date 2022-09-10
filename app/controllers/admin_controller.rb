4a026576575a2e2c90e303f2175a70c12985d0ee
Raul Riera <rieraraul@gmail.com>
2022-09-10 12:10:29 -0400

    feat: add filter support to practices

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,13 @@ class AdminController < ApplicationController
   before_action :require_superadmin
 
   def practices
+    @filter = params[:filter] || 'all'
     @total_practices = Practice.count
-    @practices = Practice.includes(:subscription).order('created_at desc').limit(250)
+
+    if @filter == 'all'
+      @practices = Practice.includes(:subscription).order('created_at desc').limit(250)
+    elsif @filter == 'active'
+      @practices = Practice.includes(:subscription).where(subscriptions: { status: 'active' }).order('created_at desc').limit(250)
+    end
   end
 end
