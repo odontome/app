28a4e46e7cd5916bfe8ad24aff4e4487de80ca10
Raul Riera <rieraraul@gmail.com>
2025-08-05 14:55:47 -0500

    fix: display the timezone in the emails

--- test/mailers/previews/patient_mailer_preview.rb
+++ test/mailers/previews/patient_mailer_preview.rb
@@ -17,4 +17,9 @@ class PatientMailerPreview < ActionMailer::Preview
     doctor = Doctor.last
     PatientMailer.appointment_scheduled_email("rieraraul@gmail.com", "Raul Riera", 12.hours.from_now, 13.hours.from_now, "Odonto Dev Practice", "es", "Eastern Time (US & Canada)", doctor, "practice@practice.com")
   end
+
+  def appointment_soon_email
+    doctor = Doctor.last
+    PatientMailer.appointment_soon_email("patient@example.com", "John Doe", 24.hours.from_now, 25.hours.from_now, "Dental Practice Example", "en", "Eastern Time (US & Canada)", doctor, "practice@example.com")
+  end
 end
