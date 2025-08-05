1dff110abbcbcee5838028827c1e3f47522dd99d
Raul Riera <rieraraul@gmail.com>
2025-08-04 22:58:44 -0500

    Add remember token fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_08_234635) do
+ActiveRecord::Schema[7.2].define(version: 2024_01_01_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -179,7 +179,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_08_234635) do
     t.integer "failed_login_count", default: 0
     t.boolean "subscribed_to_digest", default: true
     t.string "password_digest"
+    t.string "remember_token"
+    t.datetime "remember_token_expires_at"
     t.index ["perishable_token"], name: "index_users_on_perishable_token"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
