40a518db309f6fd3c1aee295a6ba4c58112db110
Raul Riera <rieraraul@gmail.com>
2025-09-23 23:56:53 -0500

    feat(appointments): allow the calendar to show patients in waiting room (#764)
    * feat(appointments): visualize patients in waiting room
    
    * precompile assets
    
    * Hide the option when its not needed

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,3 @@
 //
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-
-// Add additional link directives below for any other asset folders you need
-// Example:
-//= link_tree ../../vendor/assets/images
