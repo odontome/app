3455973f3a15309cc4167b825a83d525e5fd2f2f
Raul Riera <rieraraul@gmail.com>
2025-10-05 21:14:19 -0500

    feat(patients): support uploading patient photos

--- app/models/profile_image.rb
+++ app/models/profile_image.rb
@@ -3,6 +3,8 @@
 class ProfileImage < ApplicationRecord
   MAX_PER_PRACTICE = 500
 
+  attr_reader :file_url_for_deletion
+
   belongs_to :practice, counter_cache: true
   belongs_to :imageable, polymorphic: true
 
@@ -11,9 +13,12 @@ class ProfileImage < ApplicationRecord
   validates :imageable, presence: true
   validate :enforce_practice_upload_limit, on: :create
 
+  before_destroy :cache_file_url_for_deletion
   after_destroy_commit :delete_remote_asset
 
   private
+
+  attr_writer :file_url_for_deletion
 
   def enforce_practice_upload_limit
     return if practice.blank?
@@ -24,7 +29,14 @@ class ProfileImage < ApplicationRecord
     errors.add(:base, I18n.t('errors.messages.profile_image_limit_reached', limit: MAX_PER_PRACTICE))
   end
 
+  def cache_file_url_for_deletion
+    self.file_url_for_deletion = file_url_in_database.presence || file_url_was.presence || file_url
+  end
+
   def delete_remote_asset
-    SimpleFileUpload::DeleteFile.new(file_url: file_url).call
+    url = file_url_for_deletion.presence || file_url
+    return if url.blank?
+
+    SimpleFileUpload::DeleteFile.new(file_url: url).call
   end
 end
