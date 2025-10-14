68420a8190f2dd2ad62d0406679dfc1fcf8754ea
Raul Riera <rieraraul@gmail.com>
2025-10-14 17:42:21 -0400

    Upgrade all gems (#791)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
+Dir[Rails.root.join('test/support/**/*.rb')].sort.each { |f| require f }
+
 module ActiveSupport
   class TestCase
     fixtures :all
