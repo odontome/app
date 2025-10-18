be82c30b70354ad4cff855f2a06e22c79525ae99
Raul Riera <rieraraul@gmail.com>
2025-10-18 13:14:44 -0400

    feat: improve the new user experience  (#797)
    * feat(nux): improve the new user experience
    
    * Updated the translations
    
    * Update show.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,6 +42,38 @@ module ApplicationHelper
                             data: { "bs-toggle": 'popover', "bs-placement": 'top', "bs-content": value, "bs-html": true }
   end
 
+  def check_icon_tag(checked)
+    if checked
+      <<~SVG.html_safe
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+             viewBox="0 0 24 24" fill="none" stroke="currentColor"
+             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             class="icon icon-tabler icons-tabler-outline icon-tabler-circle-check text-green">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+          <path d="M9 12l2 2l4 -4" />
+        </svg>
+      SVG
+    else
+      <<~SVG.html_safe
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+             viewBox="0 0 24 24" fill="none" stroke="currentColor"
+             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             class="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed text-gray">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+          <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+          <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+          <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+          <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+          <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+          <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+          <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+        </svg>
+      SVG
+    end
+  end
+
   def is_current_datebook?(id)
     params[:controller] == 'datebooks' && params[:id] == id.to_s
   end
