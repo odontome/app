d1e7f56c8e3d215c55021cbe3c960156dbd44007
Raul Riera <rieraraul@gmail.com>
2025-10-29 12:00:34 -0400

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,19 +6,7 @@ class WelcomeController < ApplicationController
   def index
     flash.keep
     if current_user
-      if current_user_is_superadmin?
-        home_url = practices_admin_url
-      elsif session[:LAST_VISITED_DATEBOOK]
-        begin
-          home_url = Datebook.with_practice(current_user.practice_id).find(session[:LAST_VISITED_DATEBOOK])
-        rescue ActiveRecord::RecordNotFound
-          home_url = Datebook.with_practice(current_user.practice_id).first
-        end
-      else
-        home_url = Datebook.with_practice(current_user.practice_id).first
-      end
-
-      redirect_to home_url
+      redirect_to determine_home_url
     else
       redirect_to :signin
     end
@@ -27,4 +15,30 @@ class WelcomeController < ApplicationController
   def privacy; end
 
   def terms; end
+
+  private
+
+  def determine_home_url
+    return practices_admin_url if current_user_is_superadmin?
+
+    # Always attempt to send the user back to their most relevant datebook
+    home_datebook = last_visited_datebook || first_available_datebook
+
+    return home_datebook if home_datebook
+
+    # Fall back to pages the user is authorised to see when no datebooks exist yet
+    return new_datebook_url if user_is_admin?
+
+    patients_url
+  end
+
+  def last_visited_datebook
+    return unless session[:LAST_VISITED_DATEBOOK]
+
+    Datebook.with_practice(current_user.practice_id).find_by(id: session[:LAST_VISITED_DATEBOOK])
+  end
+
+  def first_available_datebook
+    Datebook.with_practice(current_user.practice_id).first
+  end
 end
