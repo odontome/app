df7b6d0aaa7ef3c8853e3a67237499af5857c768
Raul Riera <rieraraul@gmail.com>
2026-09-04 22:36:15 -0500

    fix(tests): make daily appointment scope timezone deterministic

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -14,50 +14,58 @@ class AppointmentTest < ActiveSupport::TestCase
     end
   end
 
-  test 'today_for_practice returns all todays appointments for practice' do
-    practice = practices(:complete)
-    doctor = doctors(:rebecca)
-    datebook = datebooks(:playa_del_carmen)
+  [
+    ['UTC', Time.utc(2026, 9, 4, 23, 30)],
+    ['America/Cancun', Time.utc(2026, 1, 4, 1, 30)]
+  ].each do |app_timezone, now|
+    test "today_for_practice uses the practice date when #{app_timezone} has a different date" do
+      Time.use_zone(app_timezone) do
+        travel_to now do
+          practice = practices(:complete)
+          doctor = doctors(:rebecca)
+          datebook = datebooks(:playa_del_carmen)
+          practice_now = Time.current.in_time_zone(practice.timezone)
+          refute_equal Time.current.to_date, practice_now.to_date
 
-    patient = Patient.create!(
-      practice: practice, firstname: 'Test', lastname: 'Today',
-      uid: 'TODAY01', date_of_birth: Date.new(1990, 1, 1)
-    )
+          patient = Patient.create!(
+            practice: practice, firstname: 'Test', lastname: 'Today',
+            uid: 'TODAY01', date_of_birth: Date.new(1990, 1, 1)
+          )
 
-    # Today's confirmed appointment
-    today_confirmed = Appointment.create!(
-      datebook: datebook, doctor: doctor, patient: patient,
-      starts_at: Time.current.change(hour: 10), ends_at: Time.current.change(hour: 10, min: 30),
-      status: Appointment.status[:confirmed]
-    )
+          # Build today's appointments in the practice timezone, not the app timezone.
+          today_confirmed = Appointment.create!(
+            datebook: datebook, doctor: doctor, patient: patient,
+            starts_at: practice_now.change(hour: 10), ends_at: practice_now.change(hour: 10, min: 30),
+            status: Appointment.status[:confirmed]
+          )
 
-    # Today's waiting room appointment
-    today_waiting = Appointment.create!(
-      datebook: datebook, doctor: doctor, patient: patient,
-      starts_at: Time.current.change(hour: 11), ends_at: Time.current.change(hour: 11, min: 30),
-      status: Appointment.status[:waiting_room]
-    )
+          today_waiting = Appointment.create!(
+            datebook: datebook, doctor: doctor, patient: patient,
+            starts_at: practice_now.change(hour: 11), ends_at: practice_now.change(hour: 11, min: 30),
+            status: Appointment.status[:waiting_room]
+          )
 
-    # Today's cancelled appointment (should be included)
-    today_cancelled = Appointment.create!(
-      datebook: datebook, doctor: doctor, patient: patient,
-      starts_at: Time.current.change(hour: 14), ends_at: Time.current.change(hour: 14, min: 30),
-      status: Appointment.status[:cancelled]
-    )
+          # Cancelled appointments are also included in the daily schedule.
+          today_cancelled = Appointment.create!(
+            datebook: datebook, doctor: doctor, patient: patient,
+            starts_at: practice_now.change(hour: 14), ends_at: practice_now.change(hour: 14, min: 30),
+            status: Appointment.status[:cancelled]
+          )
 
-    # Yesterday's confirmed appointment (should be excluded)
-    yesterday_confirmed = Appointment.create!(
-      datebook: datebook, doctor: doctor, patient: patient,
-      starts_at: 1.day.ago.change(hour: 10), ends_at: 1.day.ago.change(hour: 10, min: 30),
-      status: Appointment.status[:confirmed]
-    )
+          yesterday_confirmed = Appointment.create!(
+            datebook: datebook, doctor: doctor, patient: patient,
+            starts_at: practice_now.yesterday.change(hour: 10), ends_at: practice_now.yesterday.change(hour: 10, min: 30),
+            status: Appointment.status[:confirmed]
+          )
 
-    results = Appointment.today_for_practice(practice.id, practice.timezone)
-    result_ids = results.map(&:id)
+          result_ids = Appointment.today_for_practice(practice.id, practice.timezone).map(&:id)
 
-    assert_includes result_ids, today_confirmed.id
-    assert_includes result_ids, today_waiting.id
-    assert_includes result_ids, today_cancelled.id
-    refute_includes result_ids, yesterday_confirmed.id
+          assert_includes result_ids, today_confirmed.id
+          assert_includes result_ids, today_waiting.id
+          assert_includes result_ids, today_cancelled.id
+          refute_includes result_ids, yesterday_confirmed.id
+        end
+      end
+    end
   end
 end
